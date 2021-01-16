43261157567eb3d1032f641ea4c13ada3c90a7fe
Abdul Fatah <fatahhikami@gmail.com>
2021-01-17 06:23:15 +0700

    IdolData - Change Detail

--- app/src/main/java/com/example/dicoding_made_submission/utility/IdolData.java
+++ app/src/main/java/com/example/dicoding_made_submission/utility/IdolData.java
@@ -37,18 +37,18 @@ public class IdolData {
     };
 
     private static String[] idolDetails = {
-            "Her nickname is “Angel Chaewon”, “Fairy Chaewon”, “Raddish Cabbage Fairy” she attends Hanlim Multi Art School",
+            "Her nickname is “Angel Chaewon”, “Fairy Chaewon”, “Raddish Cabbage Fairy”.",
             "Her hometown is Yongin, South Korea , She is a former JYP Ent. trainee ",
-            "She was born in Siheung-Dong, Geumcheon-Gu, Seoul, South Korea . Woollim trainees said that Eunbi is like their mother cause of her good leadership skills",
-            "Hitomi is from Tochigi prefecture, Japan. Hitomi’s nicknames are Strawberry Princess, Hyi-Chan and Pang Hitomi",
-            "She was born in Yangsan, South Gyeongsang, South Korea. She attended Bokwang High School in Yangsan, then she transferred to Hanlim Multi Art School ",
+            "She was born in Siheung-Dong, Geumcheon-Gu, Seoul, South Korea .",
+            "Hitomi’s nicknames are Strawberry Princess, Hyi-Chan and Pang Hitomi",
+            "She was born in Yangsan, South Gyeongsang, South Korea. Attended Bokwang High School in Yangsan",
             "Her official color is a shade of white called Snow White Minju.",
-            "Nako was born in Tokyo prefecture, Japan  . Her charm point is her dimples in which she “gets from her mama.”",
-            "Sakura was born in Kagoshima City, Japan . She attended Shigakukan Middle School, a private school in Kisarazu, Japan ",
-            "She enjoys playing with her sister she when she’s home. She was one of the youngest trainees on Produce48 ",
-            "She was born in Gil-Dong, Gangdong, South Korea. She attended Hanlim Multi Art School. ",
-            "Yujin is from Daejeon but currently lives in Seoul. Her specialties are girl’s hip hop and piano",
-            "She is from Busan, South Korea. She enjoys singing, playing the guitar, keyboard, and drums."
+            "Nako was born in Tokyo prefecture, Japan",
+            "Sakura was born in Kagoshima City, Japan.",
+            "She was one of the youngest trainees on Produce48 ",
+            "She was born in Gil-Dong, Gangdong, South Korea.",
+            "Yujin is from Daejeon but currently lives in Seoul.",
+            "She is from Busan, South Korea."
     };
 
     private static int[] heroesImages = {
