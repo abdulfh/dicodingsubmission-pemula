714fa845d55f347a5e9e6c7acc7c615d67a0dd26
Abdul Fatah <fatahhikami@gmail.com>
2021-01-16 22:08:03 +0700

    Model - Add Idol Model

---
+++ app/src/main/java/com/example/dicoding_made_submission/model/IdolModel.java
@@ -0,0 +1,40 @@
+package com.example.dicoding_made_submission.model;
+
+public class IdolModel {
+    private String name;
+    private String detail;
+    private String birthday;
+    private int photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+}
